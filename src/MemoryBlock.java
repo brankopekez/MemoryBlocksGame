ab02392830df2b92382093ebc4a9e5a294da7e0c
brankopekez <branko.pekez.96@gmail.com>
2023-08-17 19:53:08 +0200

    Code input.

--- src/MemoryBlock.java
+++ src/MemoryBlock.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class MemoryBlock {
+import javax.swing.*;
+import java.awt.*;
+
+public class MemoryBlock extends JButton {
+    private final int value;
+    private boolean isFlipped;
+
+    public MemoryBlock(int value) {
+        this.value = value;
+        this.isFlipped = false;
+        setPreferredSize(new Dimension(100, 100));
+        setFont(new Font("Arial", Font.BOLD, 40));
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isFlipped() {
+        return isFlipped;
+    }
+
+    public void flip() {
+        isFlipped = true;
+        setText(String.valueOf(value));
+    }
+
+    public void unflip() {
+        isFlipped = false;
+        setText("");
+    }
 }
--- src/MemoryBlocksGame.java
+++ src/MemoryBlocksGame.java
@@ -1,2 +1,85 @@
-package PACKAGE_NAME;public class MemoryBlocksGame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MemoryBlocksGame {
+    private JFrame frame;
+    private JPanel gridPanel;
+    private MemoryBlock flippedBlock;
+    private List<MemoryBlock> blocks;
+
+    public MemoryBlocksGame() {
+        frame = new JFrame("Memory Blocks Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gridPanel = new JPanel(new GridLayout(4, 4));
+        frame.add(gridPanel, BorderLayout.CENTER);
+
+        blocks = new ArrayList<>();
+        for (int i = 1; i <= 8; i++) {
+            blocks.add(new MemoryBlock(i));
+            blocks.add(new MemoryBlock(i));
+        }
+        Collections.shuffle(blocks);
+
+        for (MemoryBlock block : blocks) {
+            gridPanel.add(block);
+        }
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void checkMatch(MemoryBlock block) {
+        if (block == flippedBlock) {
+            return;
+        }
+
+        if (flippedBlock == null) {
+            flippedBlock = block;
+        } else {
+            if (flippedBlock.getValue() == block.getValue()) {
+                flippedBlock.setEnabled(false);
+                block.setEnabled(false);
+            } else {
+                // Delay a bit before unflipping the unmatched blocks
+                MemoryBlock t = flippedBlock;
+                Timer timer = new Timer(300, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        t.unflip();
+                        block.unflip();
+                    }
+                });
+                timer.setRepeats(false);
+                timer.start();
+            }
+            flippedBlock = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MemoryBlocksGame game = new MemoryBlocksGame();
+
+                for (MemoryBlock block : game.blocks) {
+                    block.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if (!block.isFlipped()) {
+                                block.flip();
+                                game.checkMatch(block);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 }
